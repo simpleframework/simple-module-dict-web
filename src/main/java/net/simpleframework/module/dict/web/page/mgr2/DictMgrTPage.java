a461c227cfdfd03853903f97298bf8e325a30bf1
CHENKAN <CHENKAN@CHENKAN-PC>
2015-07-28 15:22:44 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/module/dict/web/page/mgr2/DictMgrTPage.java
+++ src/main/java/net/simpleframework/module/dict/web/page/mgr2/DictMgrTPage.java
@@ -1,7 +1,11 @@
 package net.simpleframework.module.dict.web.page.mgr2;
 
+import java.io.IOException;
+import java.util.Map;
+
 import net.simpleframework.module.common.web.page.AbstractMgrTPage;
 import net.simpleframework.module.dict.IDictContextAware;
+import net.simpleframework.mvc.PageParameter;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -11,4 +15,12 @@ import net.simpleframework.module.dict.IDictContextAware;
  */
 public class DictMgrTPage extends AbstractMgrTPage implements IDictContextAware {
 
+	@Override
+	protected String toHtml(final PageParameter pp, final Map<String, Object> variables,
+			final String currentVariable) throws IOException {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<div class='DictMgrTPage clearfix'>");
+		sb.append("</div>");
+		return sb.toString();
+	}
 }
